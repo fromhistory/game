40e1d084a5e57aa26199b5ce5882b429f83d8f99
fromhistory <genichka@gmail.com>
2023-04-19 20:16:43 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,46 @@
+# A Hangman game
+
+
+# Make this game for two players, so it connects to the database and pulls the score for the player (history)
+
+
+# STORE DATA IN THE FILE, SO IT COULD BE GOOD FOR THE NEXT USE
+# WHEEL OF FORTUNE: SENDS AN API CALL
+# KEEP THE LEADERSBOARD
+# LOOK UP DATA USING AN API CALL
+
+# guess a word when you can only be wrong seven times. If you are not wrong you can keep playing
+
+word = "freedom"
+
+# function to check if the guessed letter is in the word
+
+def correct_letter(guess, word):
+    return guess in word
+
+
+# Function asking for a guess
+def make_guess():
+    while True:
+        guess = input("Please guess a letter: \n")
+        if not guess.isalpha():
+            continue
+        break
+    return guess
+
+# Get to generate the display based on the length of a random word that I found using an API call
+display = [" ",  " ", " ", " ", " ", " ", " "]
+
+# Function to put the letter in a word: "freedom"
+def put_letter(letter, word, display):
+    for index, elem in enumerate(word):
+        if elem == letter:
+            display[index] = letter
+    return display
+
+letter = "e"
+
+put_letter(letter, word, display)
+
+print(display)
+
